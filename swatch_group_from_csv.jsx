1aca5f065b06adebcd9a0e90100303bc42f649f3
williams <wdjsdev@gmail.com>
2021-04-14 20:47:25 -0500

    Create swatch_group_from_csv.jsx

---
+++ swatch_group_from_csv.jsx
@@ -0,0 +1,113 @@
+#target Illustrator
+function createSwatchGroupFromCSV() {
+    var valid = true; //running flag validation. if this is ever false, exit
+    var docRef = app.activeDocument;
+    var swatches = docRef.swatches;
+    var swatchGroups = docRef.swatchGroups;
+    var destSwatchGroup; //this is the swatch group that will be created
+    var csvFileRegex = /.csv$/i;
+    var csvRows; //array of all rows in csv file
+
+
+    //function definitions
+
+    //send message to user
+    function saySomething(msg) {
+        //no need to check the operating system here
+        //alerts work the same on windows and mac
+        alert("Script message:\n" + msg);
+    }
+
+    function getCSV() {
+        var userSelection;
+
+        userSelection = File.openDialog("Select a CSV File", function (f) {
+            return f instanceof Folder || csvFileRegex.test(f);
+        })
+
+        if (!userSelection) {
+            saySomething("No CSV file selected. Aborting.");
+            valid = false;
+        }
+        return userSelection;
+    }
+
+    function readCSV() {
+        //open the csv file to read it's contents
+        //if the contents is an empty string, alert and exit
+        //else, split by \n and set the resulting array to csvRows
+        csvFile.open("r");
+        var csvContents = csvFile.read();
+        csvFile.close();
+
+        if (csvContents === "") {
+            saySomething("CSV file is blank.");
+            valid = false;
+            return;
+        }
+
+        csvRows = csvContents.replace(/["']/g,"").split("\n");
+    }
+
+    function processCSV() {
+        destSwatchGroup = swatchGroups.add();
+        destSwatchGroup.name = csvFile.name;
+
+        //loop the csvRows array
+        var curRow;
+        for (var r = 1, len = csvRows.length; r < len && valid; r++) {
+            curRow = csvRows[r];
+            processRow(curRow);
+        }
+
+    }
+
+    function processRow(row) {
+        if (row === "" ){
+            //nothing to see here.. just move on
+            return;
+        }
+
+        var columns = row.indexOf(",")>-1 ? row.split(",") : row.split(";");
+
+        var props =
+        {
+            name: columns[0],
+            r: columns[1],
+            g: columns[2],
+            b: columns[3]
+        }
+        createSwatch(props);
+    }
+
+    function createSwatch(props) {
+        var rgbColor = new RGBColor();
+        rgbColor.red = props.r;
+        rgbColor.green = props.g;
+        rgbColor.blue = props.b;
+
+        var newSwatch = swatches.add();
+        newSwatch.name = props.name;
+        newSwatch.color = rgbColor;
+
+        destSwatchGroup.addSwatch(newSwatch);
+    }
+
+    if (valid) {
+        var csvFile = getCSV();
+    }
+
+    if (valid) {
+        readCSV();
+    }
+
+    if (valid) {
+        processCSV();
+    }
+
+    if (valid) {
+        saySomething("Success!");
+    }
+
+}
+createSwatchGroupFromCSV();
